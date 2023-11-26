92e974ce8996bec8a3ff3536318c5d5c723b231b
Mykhailo Popyzenko <mp.and.sp@ukr.net>
2023-11-27 01:52:25 +0200

    added register & login

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -7,7 +7,7 @@ const getAll = async (req, res) => {
   res.json(result);
 };
 
-const getById = async (req, res, next) => {
+const getById = async (req, res) => {
   const { id } = req.params;
   const result = await Contact.findById(id);
   if (!result) {
